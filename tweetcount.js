8e1a8385f11ebe7910e1abed4c999c34e73ef641
rick4rich <68098096+rick4rich@users.noreply.github.com>
2020-07-10 03:42:54 -0700

    only in js
    visit https://rickrich.co now

---
+++ tweetcount.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+//========visit rickrich.co now================
+
+
+
+
+
+
+prompt ("Compose your tweet here");
+var tweet = tweet.length;
+alert ("your tweet exceedes the maximum count" +" "+ (200-tweet) + "remaning characters" );
+
+
+
+
+
+
+//========visit rickrich.co now================
